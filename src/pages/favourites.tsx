5375f11d36fbd514dc621bd230ad62d2fdf02d97
Christopher <christopher@Christophers-MacBook-Pro.local>
2024-04-23 22:53:30 +0700

    add favourite page

--- src/pages/favourites.tsx
+++ src/pages/favourites.tsx
@@ -1,36 +1,49 @@
-import { useRouter } from 'next/router';
-import { getFilmDetail } from '../api/hello';
+import { useState, useEffect } from 'react';
 
-const FavouritePage = ({ filmDetail }: any) => {
-    const router = useRouter()
+const FavouritePage = () => {
+  console.log("pantek")
+  let currentFavourites : any = [];
+  if (typeof window !== 'undefined') {
+    const existingFavourites = localStorage.getItem('favourites');
+    currentFavourites = JSON.parse(existingFavourites || '') || []
+  }
 
-    const {desc, duration, genre, imageLargeUrl, rating, releaseDate, starring, title, year} = filmDetail
+  const [favouritesLocalStorage, setFavouritesLocalStorage] = useState([]);
+  
+  
+  useEffect(()=> {
+    setFavouritesLocalStorage(currentFavourites)
+  },[])
+
+
+
+  const removeFromFavourites = async(ev: any, film: any) => {
+    ev.stopPropagation();
+    const idToRemove = film.id;
+    const newFavourites = favouritesLocalStorage.filter(obj => obj.id !== idToRemove);
+    localStorage.setItem('favourites', JSON.stringify(newFavourites));  
+
+    setFavouritesLocalStorage(newFavourites)
+  }
+
+  if(!favouritesLocalStorage.length) {
+    return <div>Empty</div>
+  }
   return (
-    <div>
-         <button type="button" onClick={() => router.back()}>
-      Click here to go back
-    </button>
-    <button>Like this element</button>
-        <p>{desc}</p>
-        <p>{duration}</p>
-        <p>{genre}</p>
-        <p>{rating}</p>
-        <p>{releaseDate}</p>
-        <p>{starring.map((star: string, index: any) => {
-            return <span>{star} </span>
-        })}
-        </p>
-        <p>{title}</p>
-        <p>{year}</p>
-
-     </div>)
-
+    <main>
+      {favouritesLocalStorage?.map((film: any, index: any) => {
+        return (
+          <div>
+            ahahha
+            <button onClick={(e)=> removeFromFavourites(e, film)}>Remove this element</button>
+            <p>{film.title}</p>
+            <p>{film.rating}</p>
+            <p>{film.year}</p>
+          </div>
+        )
+      })}
+     </main>
+  )
 };
 
 export default FavouritePage;
-
-
-export async function getServerSideProps(context: any) {
-
-  }
-  
